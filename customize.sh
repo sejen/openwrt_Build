f1c429566b62d4782716622c9c04720848796a45
sejen <junjie.jiang001@gmail.com>
2024-08-08 17:10:32 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -13,5 +13,3 @@ sed -i 's/192.168.1.1/192.168.1.251/g' openwrt/package/base-files/files/bin/conf
 #sed -i 's?zstd$?zstd ucl upx\n$(curdir)/upx/compile := $(curdir)/ucl/compile?g' tools/Makefile
 #sed -i 's/$(TARGET_DIR)) install/$(TARGET_DIR)) install --force-overwrite/' package/Makefile
 #sed -i 's/root:.*/root:$1$tTPCBw1t$ldzfp37h5lSpO9VXk4uUE\/:18336:0:99999:7:::/g' package/base-files/files/etc/shadow
-
-git clone https://github.com/kenzok8/small-package.git package/openwrt-packages
